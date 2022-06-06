32a62a31cd6061cdcca6fba5918cc20778068920
lelisraphael <g5raphael@hotmail.com>
2022-06-06 12:20:37 -0300

    add footer text

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ function App() {
   return (
     <div className="App">
       <header className="">
-        <h1 className="text-center mt-5 mb-5 text-success">Gerador de link do whatsapp __</h1>
+        <h1 className="text-center mt-5 mb-5 text-success">
+          Gerador de link do whatsapp __
+        </h1>
       </header>
       <body>
         <Home />
       </body>
-      <footer> </footer>
+      <footer>
+        <div className="text-center p-5">
+          <p> Gerador de link do whatsApp - 2022 </p>{" "}
+        </div>
+      </footer>
     </div>
   );
 }
